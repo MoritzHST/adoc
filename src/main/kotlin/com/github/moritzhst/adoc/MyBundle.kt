bdb9eb226cd641b0c1c4adae5d580cc9b371df31
GitHub Action <action@github.com>
2024-04-23 16:41:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/moritzhst/adoc/MyBundle.kt
+++ src/main/kotlin/com/github/moritzhst/adoc/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.moritzhst.adoc
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/moritzhst/adoc/services/MyProjectService.kt
+++ src/main/kotlin/com/github/moritzhst/adoc/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.moritzhst.adoc.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moritzhst.adoc.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
